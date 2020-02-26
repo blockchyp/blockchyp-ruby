1acd673b63d4139ba4cdfeaf349589b9f3deaa4b
BlockChyp SDK Builder <devops@blockchyp.com>
2020-02-26 00:11:43 +0000

    Merge pull request #68 from blockchyp/feature/CHYP-1158
    Add first/last name prompts

--- lib/blockchyp.rb
+++ lib/blockchyp.rb
@@ -28,6 +28,8 @@ module PromptType
   PHONE_NUMBER = 'phone'
   CUSTOMER_NUMBER = 'customer-number'
   REWARDS_NUMBER = 'rewards-number'
+  FIRST_NAME = 'first-name'
+  LAST_NAME = 'last-name'
 end
 
 module AVSResponse
