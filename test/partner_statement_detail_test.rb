1a350529e43fa87abd86d03ccfddd7fda114b512
BlockChyp SDK Builder <devops@blockchyp.com>
2023-10-30 10:39:48 -0600

    Merge pull request #253 from blockchyp/develop
    Develop

---
+++ test/partner_statement_detail_test.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+# Copyright 2019-2023 BlockChyp, Inc. All rights reserved. Use of this code is
+# governed by a license that can be found in the LICENSE file.
+#
+# This file was generated automatically by the BlockChyp SDK Generator.
+# Changes to this file will be lost every time the code is regenerated.
+
+require ::File.expand_path('test_helper', __dir__)
+
+module BlockChyp
+  class PartnerStatementDetailTest < TestCase
+    def test_partner_statement_detail
+
+      puts "Running test_partner_statement_detail..."
+
+      config = load_test_config
+
+      blockchyp = BlockChyp.new(
+        config[:apiKey],
+        config[:bearerToken],
+        config[:signingKey]
+      )
+      blockchyp.gateway_host = config[:gatewayHost]
+      blockchyp.test_gateway_host = config[:testGatewayHost]
+      blockchyp.dashboard_host = config[:dashboardHost]
+
+
+
+
+
+      # Set request parameters
+      request = {
+        test: true
+      }
+
+      response = blockchyp.partner_statement_detail(request)
+      assert_not_nil(response)
+      # response assertions
+      assert(response[:success])
+    end
+  end
+end
