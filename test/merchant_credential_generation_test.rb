4c951ad6e0c8690bc3b8a9f5b05b361945a453b3
BlockChyp SDK Builder <devops@blockchyp.com>
2024-02-01 18:13:03 +0000

    Merge pull request #260 from blockchyp/feature/javaBug
    Java test bug and php null exception fix

---
+++ test/merchant_credential_generation_test.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+# Copyright 2019-2024 BlockChyp, Inc. All rights reserved. Use of this code is
+# governed by a license that can be found in the LICENSE file.
+#
+# This file was generated automatically by the BlockChyp SDK Generator.
+# Changes to this file will be lost every time the code is regenerated.
+
+require ::File.expand_path('test_helper', __dir__)
+
+module BlockChyp
+  class MerchantCredentialGenerationTest < TestCase
+    def test_merchant_credential_generation
+
+      puts "Running test_merchant_credential_generation..."
+
+      config = load_test_config
+
+      blockchyp = BlockChyp.new(
+        config[:apiKey],
+        config[:bearerToken],
+        config[:signingKey]
+      )
+      blockchyp.gateway_host = config[:gatewayHost]
+      blockchyp.test_gateway_host = config[:testGatewayHost]
+      blockchyp.dashboard_host = config[:dashboardHost]
+
+
+
+
+
+      # Set request parameters
+      request = {
+        test: true,
+        merchantId: '<MERCHANT ID>'
+      }
+
+      response = blockchyp.merchant_credential_generation(request)
+      assert_not_nil(response)
+      # response assertions
+      assert(response[:success])
+    end
+  end
+end
