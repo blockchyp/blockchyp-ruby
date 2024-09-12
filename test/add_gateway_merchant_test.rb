76c9a49b8f490f5e10a5c1abc8cc9f2c51c9169b
BlockChyp SDK Builder <devops@blockchyp.com>
2024-09-12 15:49:34 +0000

    Merge pull request #281 from blockchyp/develop
    Develop to Master

---
+++ test/add_gateway_merchant_test.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+# Copyright 2019-2024 BlockChyp, Inc. All rights reserved. Use of this code is
+# governed by a license that can be found in the LICENSE file.
+#
+# This file was generated automatically by the BlockChyp SDK Generator.
+# Changes to this file will be lost every time the code is regenerated.
+
+require ::File.expand_path('test_helper', __dir__)
+
+module BlockChyp
+  class AddGatewayMerchantTest < TestCase
+    def test_add_gateway_merchant
+
+      puts "Running test_add_gateway_merchant..."
+
+      config = load_test_config
+
+      blockchyp = BlockChyp.new(
+        config[:apiKey],
+        config[:bearerToken],
+        config[:signingKey]
+      )
+      blockchyp.gateway_host = config[:gatewayHost]
+      blockchyp.test_gateway_host = config[:testGatewayHost]
+      blockchyp.dashboard_host = config[:dashboardHost]
+
+
+      profile = config[:profiles][:partner]
+      blockchyp = BlockChyp.new(
+        profile[:apiKey],
+        profile[:bearerToken],
+        profile[:signingKey]
+      )
+      blockchyp.gateway_host = config[:gatewayHost]
+      blockchyp.test_gateway_host = config[:testGatewayHost]
+      blockchyp.dashboard_host = config[:dashboardHost]
+
+
+
+      # Set request parameters
+      request = {
+        profile: {
+          dbaName: 'DBA Name',
+          companyName: 'Corporate Entity Name'
+        }
+      }
+
+      response = blockchyp.add_gateway_merchant(request)
+      assert_not_nil(response)
+      # response assertions
+      assert(response[:success])
+    end
+  end
+end
